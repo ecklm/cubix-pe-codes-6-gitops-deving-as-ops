ceefdbde8905838b074772ce9a187878566c69a9
Máté Eckl <ecklm94@gmail.com>
2026-09-19 15:10:57 +0200

    fix(certmanager): Use let's encrypt staging CA

--- cert-manager.tf
+++ cert-manager.tf
@@ -108,7 +108,7 @@ resource "helm_release" "letsencrypt_cluster_issuer" {
                     spec:
                       acme:
                         email: ecklm@cubix-pe.hu
-                        server: https://acme-v02.api.letsencrypt.org/directory
+                        server: https://acme-staging-v02.api.letsencrypt.org/directory
                         privateKeySecretRef:
                           name: letsencrypt-account-key
                         solvers:
